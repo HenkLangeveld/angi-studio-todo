e2f93a75016e16e310eb7b4d889b1e248a442015
Henk <57254709+HenkLangeveld@users.noreply.github.com>
2022-01-05 14:56:37 +0100

    add done and edit features to todo list

--- app.js
+++ app.js
@@ -1,14 +1,54 @@
-Vue.createApp({
+const app = Vue.createApp({
   data() {
     return {
-      toDos: [],
-      enteredValue: ''
+      newToDo: '',
+      beforeEditCache: '',
+      idForToDo: 1,
+      toDos: []
     };
   },
+  directives: {
+    focus: {
+      inserted: function (el) {
+        el.focus()
+      }
+    }
+  },
+  // computed: {
+
+  // },
   methods: {
     addToDo() {
-      this.toDos.push(this.enteredValue);
-      this.enteredValue = '';
+      if(this.newToDo.trim().length === 0) {
+        return
+      }
+    this.toDos.push({
+      id: this.idForToDo,
+      title: this.newToDo,
+      done: false,
+      editing: false
+    });
+      this.newToDo = '';
+      this.idForToDo++;
+    },
+    removeToDo(index) {
+      this.toDos.splice(index, 1);
+    },
+    editToDo(toDo) {
+      this.beforeEditCache = toDo.title
+      toDo.editing = true;
+    },
+    doneEdit(toDo) {
+      if(toDo.title.trim() === '') {
+        toDo.title = this.beforeEditCache;
+      }
+      toDo.editing = false;
+    },
+    cancelEdit(toDo) {
+      toDo.title = this.beforeEditCache;
+      toDo.editing = false;
     }
   }
-}).mount('#app');
+});
+
+app.mount('#app');
